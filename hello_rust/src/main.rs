2542ea6a19adb18106b519ff17d83ab1511dadb7
anchor_cable <forever.blue.moon@gmail.com>
2023-04-08 18:47:14 +0900

    refactor: remove unuse lines

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,11 +1,8 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 use tower::ServiceBuilder;
-// use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 
-#[derive(Clone)]
-struct State {}
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
@@ -13,7 +10,6 @@ async fn hello_world() -> &'static str {
 
 #[tokio::main]
 async fn main() {
-    // let app = Router::new().route("/", get(hello_world));
     let app = Router::new()
         .route("/", get(hello_world))
         .route("/hello", get(hello_world))
