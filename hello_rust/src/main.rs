8af8a0d7e7e391235a57f53fc22f4a0e8f0d0b37
anchor_cable <forever.blue.moon@gmail.com>
2023-04-08 18:44:00 +0900

    feat: implement simple hello world API using axum

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tower::ServiceBuilder;
+// use tower_http::{compression::CompressionLayer, trace::TraceLayer};
+use tower_http::{compression::CompressionLayer, trace::TraceLayer};
+
+#[derive(Clone)]
+struct State {}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    // let app = Router::new().route("/", get(hello_world));
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/hello", get(hello_world))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(CompressionLayer::new()));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
